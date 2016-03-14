09983a79887c24aff0cf74b2658723ab13d4565f
Adam Wolski <miniukof@gmail.com>
2016-03-14 18:47:05 +0100

    Make command more functional.

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -1,6 +1,6 @@
 //! http://gameprogrammingpatterns.com/command.html
 //! Not much problem with this one in rust.
-//! We still can't return generic function though so we have to Box it.
+//! We still can't return closure though so we have to Box it.
 
 struct Player {
     pub x: i32,
@@ -8,29 +8,29 @@ struct Player {
 }
 
 impl Player {
-    fn move_to(&self, x: i32, y: i32) {
+    fn move_to(&mut self, x: i32, y: i32) {
         println!("Moving player to: ({}, {})", x, y);
         self.x = x;
         self.y = y;
     }
 }
 
-fn make_move_unit_cmd(unit: Player, x: i32, y:i32) -> Box<Fn()> {
-    Box::new(move || unit.move_to(x, y))
+fn make_move_unit_cmd(x: i32, y:i32) -> Box<Fn(&mut Player)> {
+    Box::new(move |unit: &mut Player| unit.move_to(x, y))
 }
 
 #[cfg(test)]
 mod tests {
-    use super::command::*;
+    use super::{ Player, make_move_unit_cmd};
 
     #[test]
     pub fn visual() {
         println!("\n---------------------------");
         println!("Command test.\n");
-        let unit = Player{x: 0, y: 0};
-        let cmd = make_move_unit_cmd(unit, 5, 10);
+        let mut unit = Player {x: 0, y: 0};
+        let cmd = make_move_unit_cmd(5, 10);
+        cmd(&mut unit);
         assert!(unit.x == 5);
         assert!(unit.y == 10);
-        cmd();
     }
 }
