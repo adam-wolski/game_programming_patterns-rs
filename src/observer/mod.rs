05d62423d23088a684c511e82d6e1327130fd410
Adam Wolski <miniukof@gmail.com>
2016-03-17 18:39:00 +0100

    Working observer pattern.

--- src/observer/mod.rs
+++ src/observer/mod.rs
@@ -1,26 +1,33 @@
+//! Observer Pattern
 //! http://gameprogrammingpatterns.com/observer.html
 
+// Used for holding observers.
+use std::cell::RefCell;
+use std::rc::Rc;
+
+// ================================================================================================
+// Defines that we need to get out of our way for examples.
+// ================================================================================================
+
+/// Types of events that can be sent by observer subjects.
 #[derive(Clone, Copy)]
-enum Event {
+pub enum Event {
     EntityFell,
 }
 
-
-enum Achievement {
+/// Types of achievements that can be unlocked.
+#[derive(PartialEq)]
+pub enum Achievement {
     FellOfTheBridge,
 }
 
-
-pub trait Observer {
-    fn on_notify<E: Entity>(&self, entity: &E, event: Event);
-}
-
-
+/// Base trait of Entity in our theoretical game.
 pub trait Entity {
     fn is_hero(&self) -> bool;
 }
 
-
+/// Our Hero that will fall of the bridge.
+#[derive(Debug, Default)]
 pub struct Hero;
 
 impl Hero {
@@ -36,106 +43,176 @@ impl Entity for Hero {
 }
 
 
-#[derive(PartialEq)]
+/// Achievements structure that will become our observer. 
+/// Well it will have it's own observer. Expained in AchievementObserver struct.
+#[derive(Debug, Default)]
 pub struct Achievements {
-    hero_on_bridge: bool,
+    pub hero_fallen: bool,
 }
 
 impl Achievements {
     pub fn new() -> Achievements {
-        Achievements { hero_on_bridge: true }
+        Achievements {
+            hero_fallen: false,
+        }
     }
-    fn unlock(&self, achievement: Achievement) {
+
+    fn unlock(&mut self, achievement: Achievement) {
         match achievement {
-            Achievement::FellOfTheBridge => println!("Fall of the bridge achievement unlocked."),
+            Achievement::FellOfTheBridge => {
+                if !self.hero_fallen {
+                    self.hero_fallen = true;
+                    println!("Fall of the bridge achievement unlocked.");
+                }
+            }
+            // Other achievements...
         }
     }
 }
 
-impl Observer for Achievements {
-    fn on_notify<E: Entity>(&self, entity: &E, event: Event) {
+/// Physics will hold subjects which we can observe and make our hero fall.
+#[derive(Default)]
+pub struct Physics {
+    fall_event: EntityFallSubject,
+}
+
+
+// ================================================================================================
+// Pattern itself
+// ================================================================================================
+
+/// Observer type that will be held inside Subject.
+pub type ObserverType = Rc<RefCell<Box<Observer>>>;
+
+
+/// Trait that defines objects which are interested in listening for events.
+pub trait Observer {
+    fn on_notify(&mut self, entity: &Entity, event: Event);
+    /// Id string used to compare with other observers. Mainly for deleting that observer.
+    fn id(&self) -> &String;
+}
+
+/// If we try to hold achievements struct itself in the Subject as a Observer, it will be tricky to 
+/// get access to it in rust. If we hold it a trait object will have get it back and downcast it.
+/// If we hold it as generic... Then we have to deal with storing generic which can be any size.
+/// Creating separate Observer is easiest way I found.
+pub struct AchievementObserver {
+    // Unlocked Achievements
+    unlocked: Vec<Achievement>,
+    achievements: Rc<RefCell<Achievements>>,
+    id: String,
+}
+
+impl AchievementObserver {
+    pub fn new(achievements: Rc<RefCell<Achievements>>) -> AchievementObserver {
+        AchievementObserver {
+            unlocked: Vec::new(),
+            achievements: achievements,
+            id: "AchievmentObserver".to_owned(),
+        }
+    }
+
+    pub fn is_unlocked(&self, achievement: Achievement) -> bool {
+        for a in &self.unlocked {
+            if *a == achievement {
+                return true;
+            }
+        }
+        false
+    }
+}
+
+impl Observer for AchievementObserver {
+    fn on_notify(&mut self, entity: &Entity, event: Event) {
         match event {
             Event::EntityFell => {
-                if entity.is_hero() && self.hero_on_bridge {
-                    self.unlock(Achievement::FellOfTheBridge)
+                if entity.is_hero() && !self.is_unlocked(Achievement::FellOfTheBridge) {
+                    self.unlocked.push(Achievement::FellOfTheBridge);
+                    self.achievements.borrow_mut().unlock(Achievement::FellOfTheBridge);
                 }
-                // Handle other events..
+            // Handle other events...
             }
         }
     }
+
+    fn id(&self) -> &String {
+        &self.id
+    }
 }
 
-pub trait Subject<'a, T: Observer>{
-    fn add_observer(&mut self, observer: &'a T);
-    fn remove_observer(&mut self, observer: &'a T);
-    fn notify<E: Entity>(&self, entity: &E, event: Event);
+
+/// Subject trait defines objects that will can be observer. It holds list of observers and sends 
+/// notifications to them.
+pub trait Subject{
+    fn add_observer(&mut self, observer: ObserverType);
+    fn remove_observer(&mut self, observer: ObserverType);
+    fn notify(&mut self, entity: &Entity, event: Event);
 }
 
-pub struct EntityFallSubject<'a, T: 'a> {
-    observers: Vec<&'a T>,
+
+/// We'll make Physics hold Subjects that can be observed instead of being a subject.
+#[derive(Default)]
+pub struct EntityFallSubject {
+    pub observers: Vec<ObserverType>,
 }
 
-impl<'a, T> EntityFallSubject<'a, T> where T: Observer
-{
-    pub fn new() -> EntityFallSubject<'a, T> {
+impl EntityFallSubject {
+    pub fn new() -> EntityFallSubject {
         EntityFallSubject { observers: Vec::new() }
     }
 }
 
-impl<'a, T> Subject<'a, T> for EntityFallSubject<'a, T> where T: Observer + PartialEq
-{
-    fn add_observer(&mut self, observer: &'a T) {
+impl Subject for EntityFallSubject {
+    fn add_observer(&mut self, observer: ObserverType) {
         self.observers.push(observer);
     }
-    fn remove_observer(&mut self, observer: &'a T) {
+    fn remove_observer(&mut self, observer: ObserverType) {
         for (i, o) in self.observers.clone().into_iter().enumerate() {
-            if o == observer {
+            if o.borrow().id() == observer.borrow().id() {
                 self.observers.remove(i);
             }
         }
     }
-    fn notify<E: Entity>(&self, entity: &E, event: Event) {
+    fn notify(&mut self, entity: &Entity, event: Event) {
         for o in &self.observers {
-            o.on_notify(entity, event);
+            o.borrow_mut().on_notify(entity, event);
         }
     }
 }
 
-
-pub struct Physics<'a, T: 'a + Observer> {
-    fall_event: EntityFallSubject<'a, T>,
-}
-
-impl<'a, T> Physics<'a, T> where T: Observer + PartialEq
-{
-    pub fn new() -> Physics<'a, T> {
+// And finally our physics implementation. Which holds fall event which can be observed.
+impl Physics {
+    pub fn new() -> Physics {
         let fall_event = EntityFallSubject::new();
         Physics { fall_event: fall_event }
     }
 
-    pub fn update_entity<E: Entity>(&self, entity: &E) {
-        // To some physics...
+    pub fn update_entity<E: Entity>(&mut self, entity: &E) {
+        // Do some physics...
         // Entity has fallen of the bridge
         self.fall_event.notify(entity, Event::EntityFell);
     }
 
-    pub fn fall_event(&mut self) -> &mut EntityFallSubject<'a, T> {
+    pub fn fall_event(&mut self) -> &mut EntityFallSubject {
         &mut self.fall_event
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{Hero, Achievements, Physics, Subject};
+    use super::{Hero, Achievements, Physics, Subject, Observer, AchievementObserver};
+    use std::cell::RefCell;
+    use std::rc::Rc;
 
     #[test]
     pub fn observer() {
-        println!("\n---------------------------");
-        println!("Command test.\n");
         let hero = Hero::new();
-        let achievements = Achievements::new();
+        let achievements = Rc::new(RefCell::new(Achievements::new()));
+        let a_observer = AchievementObserver::new(achievements.clone());
+        let a_observer = Rc::new(RefCell::new(Box::new(a_observer) as Box<Observer>));
         let mut physics = Physics::new();
-        physics.fall_event().add_observer(&achievements);
+        physics.fall_event().add_observer(a_observer);
         physics.update_entity(&hero);
+        assert!(achievements.borrow().hero_fallen == true);
     }
 }
